cf66df486d04b894c899d58d37cbf669cab6b071
Adam Christie <fractos@gmail.com>
2017-03-14 12:10:22 +0000

    homogenising to use /opt/elucidate. making mkdir, tar extract dependent on wget success.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM postgres:9.4
 
 RUN apt-get update && apt-get install -y wget
-RUN wget -O /opt/elucidate-server.tar.gz https://github.com/dlcs/elucidate-server/archive/1.0.1.tar.gz
-RUN mkdir /opt/elucidate-server && tar -xzvf /opt/elucidate-server.tar.gz --strip-components=1 -C /opt/elucidate-server
+RUN wget -O /opt/elucidate-server.tar.gz https://github.com/dlcs/elucidate-server/archive/1.0.1.tar.gz \
+	&& mkdir /opt/elucidate \
+	&& tar -xzvf /opt/elucidate-server.tar.gz --strip-components=1 -C /opt/elucidate
 COPY setup-database.sh /docker-entrypoint-initdb.d/.
 
 EXPOSE 5432
--- setup-database.sh
+++ setup-database.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-psql -d postgres -a -f /opt/elucidate-server/elucidate-db-scripts/group_roles/annotations_role.sql \
-        && psql -d postgres -a -f /opt/elucidate-server/elucidate-db-scripts/login_roles/annotations_user.sql \
-        && psql -d postgres -a -f /opt/elucidate-server/elucidate-db-scripts/annotations.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/sequences/annotation_collection_id_seq.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/sequences/annotation_id_seq.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/tables/annotation_collection.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/tables/annotation.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/views/annotation_collection_get.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/views/annotation_get.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/functions/annotation_collection_create.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/functions/annotation_create.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/functions/annotation_delete.sql \
-        && psql -d annotations -a -f /opt/elucidate-server/elucidate-db-scripts/functions/annotation_update.sql
+psql -d postgres -a -f /opt/elucidate/elucidate-db-scripts/group_roles/annotations_role.sql \
+        && psql -d postgres -a -f /opt/elucidate/elucidate-db-scripts/login_roles/annotations_user.sql \
+        && psql -d postgres -a -f /opt/elucidate/elucidate-db-scripts/annotations.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/sequences/annotation_collection_id_seq.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/sequences/annotation_id_seq.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/tables/annotation_collection.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/tables/annotation.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/views/annotation_collection_get.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/views/annotation_get.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/functions/annotation_collection_create.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/functions/annotation_create.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/functions/annotation_delete.sql \
+        && psql -d annotations -a -f /opt/elucidate/elucidate-db-scripts/functions/annotation_update.sql
 
